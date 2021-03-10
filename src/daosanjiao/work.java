d84921f5a04950161b7152b04c1dd324ad0474be
Admin <Admin@PS2019AWCDMIQX>
2021-03-10 22:20:12 +0800

    倒三角

---
+++ src/daosanjiao/work.java
@@ -0,0 +1,27 @@
+package daosanjiao;
+
+import java.util.Scanner;
+
+public class work {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		Scanner scan = new Scanner(System.in);
+		System.out.println(" ‰»Î≤„ ˝");
+		n = scan.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=0;j<i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int k=1;k<=n+1-i;k++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
